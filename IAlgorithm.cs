3987a6a52acda54d3c764df52cd9d0dc7566d283
Piotr Markowski <markowskipriv@gmail.com>
2019-10-02 19:26:32 +0200

    Created basic interface for general algorithms and stricte for Electre III

---
+++ IAlgorithm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElectreAp
+{
+    interface IAlgorithm
+    {
+        void CreateMatrixBasedOnTable();
+        void DivideThresholdsToLists();
+        void CreateMatrixForAlternativeData();
+        void CreateConcordanceSets();
+        void ShowConcordanceSets();
+        void CreateConcordanceMatrix(List<Double[][]> listOfConcordanceSets, int numberOfAlternatives);
+        void ShowConcordanceMatrix();
+        void AddMathFormsImgToList(String path);
+        void CreateTable(int numberOfAlternatives, int numberOfCriterions);
+        void CreateDiscordanceSets();
+        void ShowDiscordanceSets();
+        void ReadDataFromFileToTable(string path);
+        void SaveDataToExelFile(string path);
+        void SaveTableToExelFile(string path);
+        void DoCalculations();
+        void ValidDataInTextBox();
+        void ChoisePath();
+
+    }
+}
---
+++ IElectreIII.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElectreAp
+{
+    interface IElectreIII : IAlgorithm
+    {
+        double DoInvers(Double symbolA, Double symbolB, int numberOfSymbol);
+        void CalculateThreshold(Double[][][] table, Double value, int i, int mod);
+        void CreateOutrankingSets(List<Double[][]> listOfOutrankingSets);
+        void ShowOutrankingSets();
+        void DoStageFirst();
+        void ShowStageFirst();
+        void DoStageSecond();
+        void ShowStageSecond();
+        double CalculateSDeltaK(double deltaLast);
+        void DoStepSecond(Double[][] workingMatrix, Boolean typeOfDistillation, List<Int32> workingListOfNumbersOfOptions);
+        void DoStepFourth(Double deltaLast, Double[][] workingMatrix, Boolean typeOfDistillation, List<Int32> workingListOfNumbersOfOptions);
+        void DoStepFifth(Double lastDelta, Double[][] workingMatrix, Boolean typeOfDistillation, List<Int32> workingListOfNumbersOfOptions);
+        void DoStepSixth(Double[][] ratingMatrix, Double[][] workingMatrix, Boolean typeOfDistillation, List<Int32> workingListOfNumbersOfOptions);
+        void DoStepSeventh(Double[][] ratingMatrix, double qualificationOfTheBestOption, Double[][] workingMatrix, Boolean typeOfDistillation, List<Int32> workingListOfNumbersOfOptions);
+        void FindMin();
+        void CreateFinalRanking(Double[][] tabSum);
+        void ExchangingListOfRanksToTable(List<List<Int32>> listOfRanks);
+        void CheckingIsThereThisSameOptions(int y);
+        Double[][] CreateTabOfDistillation(String[][] placesOfOptionsAfterDistillation, int valueX, int valueY, int valueZ);
+        void ShowTableDataOfDistillation(Double[][] tab);
+        void CreateSumTableOfDistillations(Double[][] tabTopDownDistillation, Double[][] tabUpwardDistillation);
+        void ShowOptionRankingOfDistillation(String[][] rankingOfOptionsAfterDistillation);
+        void FindMax(String[][] rankingOfOptionsAfterDistillation);
+        void Exchange(int max, int min);
+    }
+}
